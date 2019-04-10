6f3a14d4c93e577a42701a42f3a637c0d7871b1f
ty61271 <ty61271@yahoo.com.tw>
2019-04-10 23:52:05 +0800

    ch3-5 (Learn) Locat、Toast、對話框

--- app/src/main/java/com/west/pratice/guess/MainActivity.kt
+++ app/src/main/java/com/west/pratice/guess/MainActivity.kt
@@ -3,6 +3,7 @@ package com.west.pratice.guess
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,10 +13,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "secret: ${secretNumber.secretNumber}")
     }
 
     fun check(view: View) {
         val n = number.text.toString().toInt()
         Log.d("MainActivity", "number : $n")
+        val diff = secretNumber.validate(n)
+        val message = when {
+            diff < 0 -> "Bigger"
+            diff > 0 -> "Smaller"
+            else -> "Yes, you got it"
+        }
+//        Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+
+        AlertDialog.Builder(this)
+            .setTitle("Message")
+            .setMessage(message)
+            .setPositiveButton("OK", null)
+            .show()
     }
 }
