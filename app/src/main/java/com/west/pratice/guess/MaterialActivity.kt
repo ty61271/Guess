2c8c66bb72f1233c68a1f590ee04d5843101e4fb
ty61271 <ty61271@yahoo.com.tw>
2019-04-11 22:26:20 +0800

    ch3-8 material 實感設計

---
+++ app/src/main/java/com/west/pratice/guess/MaterialActivity.kt
@@ -0,0 +1,54 @@
+package com.west.pratice.guess
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_material.*
+import kotlinx.android.synthetic.main.content_material.*
+
+class MaterialActivity : AppCompatActivity() {
+    val secretNumber = SecretNumber()
+    val TAG = MaterialActivity::class.java.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_material)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            AlertDialog.Builder(this)
+                .setTitle("Replay game")
+                .setMessage("Are you sure?")
+                .setPositiveButton(getString(R.string.ok),{dialog, which ->
+                    secretNumber.reset()
+                    counter.setText(secretNumber.count.toString())
+                    number.setText("")
+                })
+                .setNeutralButton("Cancel",null)
+                .show()
+        }
+        counter.setText(secretNumber.count.toString())
+    }
+
+    fun check(view: View) {
+        val n = number.text.toString().toInt()
+        Log.d(TAG, "number:" + n)
+        val diff = secretNumber.validate(n)
+        var message = getString(R.string.yes_you_got_it)
+        if (diff < 0) {
+            message = getString(R.string.bigger)
+        } else if (diff > 0) {
+            message = getString(R.string.smaller)
+        }
+        counter.setText(secretNumber.count.toString())
+//        Toast.makeText(this,message, Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this)
+            .setTitle(getString(R.string.dialog_title))
+            .setMessage(message)
+            .setPositiveButton(getString(R.string.ok), null)
+            .show()
+    }
+}
